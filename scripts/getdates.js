836487b0a6fea416659f70f383607f6cc16070d5
darwingt007 <dtorrejoncontreras@byupathway.edu>
2025-11-04 23:05:45 -0400

    added second p in footer

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,6 @@
+const currentYear =
+    document.querySelector("#currentYear");
+currentYear.textContent = new Date().getFullYear();
+
+
 document.getElementById("lastModified").innerHTML = document.lastModified;
