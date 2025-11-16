adda8621884b872cce720c7af0b6fa7de7cc6ac5
darwingt007 <dtorrejoncontreras@byupathway.edu>
2025-11-15 20:09:11 -0400

    Completed draft

---
+++ scripts/place.js
@@ -0,0 +1,3 @@
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent = "last Updated: " + document.lastModified;
---
+++ week02/scripts.js/bom.js
@@ -0,0 +1,18 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('');
+
+const li = document.createElement('li');
+
+const deleteButton = document.createElement('button');
+
+li.textContent = input.Value;
+
+deleteButton.textContent = '❌'
+
+
+li.append(deleteButton);
+
+list.append(li);
+
+
